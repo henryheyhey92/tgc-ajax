0a1939e546edfd2c902beff9506773edba239b3d
Henry Lai <e0032134@u.nus.edu>
2022-02-10 08:52:39 +0000

    add Q4

---
+++ hands-on/q4-simple-json-example/script.js
@@ -0,0 +1,17 @@
+let btn = document.querySelector('#load-image-btn');
+let content = document.querySelector('#artwork');
+
+btn.addEventListener('click', async function(){
+    let response = await axios.get('artwork.json');
+
+    console.log(response.data);
+    let h1 = document.createElement('h1');
+    let img = document.createElement('div');
+    h1.innerHTML = response.data.title;
+    img.innerHTML = `<img src=${response.data.image_url} width=\"400px\" height=\"400px\">`;
+    img.className = response.data.class;
+
+    content.appendChild(h1);
+    content.appendChild(img);
+
+})
